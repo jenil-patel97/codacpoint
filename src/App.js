7e2d650ef4c75714c1a15645a0f6a9efcc581c86
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-09-14 18:39:35 +0530

    feat: Hero

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <Hero />
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Artboard from "../assets/Artboard.svg";
+
+const Hero = () => {
+  return (
+    <div className="max-w-[100%] mx-auto grid md:grid-cols-2 header-1">
+      <img className="w-[500px] mx-auto my-4" src={Artboard} alt="artboard" />
+      <div className="flex flex-col justify-center">
+        <p className="text-[#1A1E3F] text-[60px] font-bold ">Web Developer</p>
+        <p className="my-[20px]">
+          Codacpoint helps you with intutive website solutions to <br /> beacome
+          the digital face in the industry and hunt <br /> down the global
+          market!
+        </p>
+        <button className="bg-[#0B72FE] text-white w-[200px] rounded-[23px] my-4 md:mx-0 py-3">
+          Explore More
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
